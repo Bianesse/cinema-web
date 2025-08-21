bbee12218b84e6116502e8757e5a1a01f0d87219
Bianesse <ibrahimkasabian@gmail.com>
2025-08-21 10:56:37 +0700

    fix: params props now use promise

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -1,15 +1,8 @@
 import MovieDetail from "./MovieDetails"; // This will be a client component
 import { notFound } from "next/navigation";
 
-type PageProps = {
-    params: {
-        id: string;
-    };
-};
-
-
-export default async function Page({ params }: PageProps) {
-    const { id } = params;
+export default async function Page({ params }: { params: Promise<{ id: string }> }) {
+    const { id } = await params;
 
     const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/api/movie/${id}/info`,
